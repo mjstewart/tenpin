838ab04869f8791f96cdcc79e378d416c9a8d76d
Matt Stewart <smtp.matt@gmail.com>
2019-04-12 08:30:08 +1000

    establish basic helper functions

--- src/tenpin/core.clj
+++ src/tenpin/core.clj
@@ -1,7 +1,48 @@
-(ns tenpin.core
-  (:gen-class))
+(ns tenpin.core)
+
+(defn legal-roll? [roll]
+  (and (>= roll 0) (<= roll 10)))
+
+(defn two-rolls? [[roll-1 roll-2]]
+  (and (legal-roll? roll-1) (legal-roll? roll-2)))
+
+(defn strike? [[roll-1 roll-2]]
+  (and (== roll-1 10) (== roll-2 -1)))
+
+(defn frame-score-satisfies? [frame f]
+  (and (two-rolls? frame) (f (reduce + frame) 10)))
+
+(defn spare? [frame]
+  (frame-score-satisfies? frame ==))
+
+(defn open? [frame]
+  (frame-score-satisfies? frame <))
+
+(defn spare-or-open? [frame]
+  (or (spare? frame) (open? frame)))
+
+(defn frame-complete? [frame]
+  (or (open? frame) (spare? frame) (strike? frame)))
+
+ 
+
+;; (split-at 9 c)
+
+;; overfil after 10 is 1 element list of every next throw to calculate score for 10
+(def c [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [6 4] [7 3]        [3 -1]  [-1 -1]])
+(def d [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [10 -1] [10 -1]    [10 -1] [10 -1]])
+(def e [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [10 -1] [10 -1]    [0 0]   [0 0]])
+
+
+(defn create-overfill []
+  [[0 0] [0 0]])
+
+;; todo how to make this annon lambda? or just make it 12 . 10 + optional 2 overflows for strike on 10th
+(defn create-scorecard []
+  (concat (repeatedly 10 (fn [] [-1 -1])) (create-overfill)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  (println (calculate c)))
--- test/tenpin/core_test.clj
+++ test/tenpin/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [tenpin.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest strike
+  (testing "strike when first roll is 10"
+    (is (true? (strike? [10 0])))
+    (is (true? (strike? [10 1])))
+    (is (true? (strike? [10 10])))
+    (is (true? (strike? [10 10 10 10])))
+    (is (false? (strike? [0 0])))
+    (is (false? (strike? [0 1])))
+    (is (false? (strike? [5 5])))
+    (is (false? (strike? [3 4])))
+    (is (false? (strike? [0 10])))))
+
+(deftest spare
+  (testing "spare when 2 rolls sum to 10"
+    (is (true? (spare? [10 0])))
+    (is (true? (spare? [0 10])))
+    (is (true? (spare? [5 5])))
+    (is (true? (spare? [1 9])))
+    (is (true? (spare? [9 1])))
+    (is (true? (spare? [9 1 1])))
+    (is (true? (spare? [10 0 10])))
+    (is (false? (spare? [0 0])))
+    (is (false? (spare? [1 0])))
+    (is (false? (spare? [0 1])))
+    (is (false? (spare? [10])))
+    (is (false? (spare? [3 4])))))
