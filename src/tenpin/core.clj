a9f244358ca06683f01973944485684c8485cd82
Matt Stewart <smtp.matt@gmail.com>
2019-04-14 23:01:21 +1000

    updated tests

--- src/tenpin/core.clj
+++ src/tenpin/core.clj
@@ -10,7 +10,7 @@
 (def ^:const total-frames 12)
 
 (defn legal-roll? [roll]
-  "A single roll score is legal if its between 0 and 10 inclusive"
+  "A single roll is legal if its between 0 and 10 inclusive"
   (and (>= roll 0) (<= roll 10)))
 
 (defn two-legal-rolls? [[roll1 roll2]]
@@ -27,79 +27,88 @@
 
 (defn spare? [frame]
   "both rolls must be between 0 and 10 inclusive and sum to 10"
-  (frame-score-satisfies? frame ==))
+  (-frame-score-satisfies? frame ==))
 
 (defn open? [frame]
   "both rolls must be between 0 and 10 inclusive and sum to less than 10"
-  (frame-score-satisfies? frame <))
+  (-frame-score-satisfies? frame <))
 
 (defn valid-frame? [frame]
   "a frame is valid when its either an open, spare or strike frame"
   (or (open? frame) (spare? frame) (strike? frame)))
 
-(defn score-frame [scorecard nth frame]
-  "associates the nth frame with a new score and returns the updated scorecad"
-  (assoc scorecard nth frame))
-
-(defn resolve-frame-score
-  "Calculates the first frames score by looking ahead x number of rolls. If no score can be calculated, -1 is returned"
+(defn find-next-rolls
+  "Calculates the first frames score by looking ahead x number of rolls. If no score can be calculated, [] is returned"
   ([[head & _ :as frames]]
    (cond
-     (or (strike? head) (spare? head)) (resolve-score frames 3)
-     (open? head) (resolve-score frames 2)
-     :else missing))
+     (or (strike? head) (spare? head)) (find-next-rolls frames 3)
+     (open? head) (find-next-rolls frames 2)
+     :else []))
   ([frames n]
-     "Example for resolving the score of a strike
-     resolve-score ([10 -1] [10 -1] [10 -1]) 3
+   "Example for resolving the score of a strike
+   find-next-rolls ([10 -1] [10 -1] [10 -1]) 3
 
-     [[10 -1] [10 -1] [10 -1]]
-     [10 10 10] (after flatten, filtering the missing placeholders)
-     strike requires 3 rolls to compute the first frame, otherwise its implied more frames need to be available
-     "
-     (->> frames
-          (flatten)
-          (filter #(not= missing %))
-          (take n)
-          (#(if (== (count %) n) (reduce + %) missing)))))
+   [10 10 10] (after flatten and filtering the missing placeholders)
+   strike requires 3 rolls to compute the first frames score, otherwise its implied more frames need to be available
+   "
+   (->> frames
+        (flatten)
+        (filter #(not= missing %))
+        (take n)
+        (#(if (== (count %) n) % [])))))
 
- 
-(defn pretty-frame [frame]
-  "pretty prints a frame"
-  (cond
-    (strike? frame) "X"
-    (spare? frame) (str (first frame) " /")
-    :else (str/join "," frame)))
+(defn symbolize-frame10 [[head & rest :as all]]
+  "frame 10 includes all fill frames that have counted towards the score"
+  (str/trim (if (nil? head)
+              ""
+              (cond
+                (== head 10) (str "X " (symbolize-frame10 rest))
+                (== 10 (reduce + 0 (take 2 all))) (str head " / "  (symbolize-frame10 (drop 2 all)))
+                :else (str head " " (symbolize-frame10 rest))))))
 
-(defn full-scorecard [scorecard]
-  (let [sliding-windows (partition 3 1 scorecard)]
-    (map (fn [[head & _ :as frames]]
-           {:score (resolve-frame-score frames) :symbol (pretty-frame head)}) sliding-windows)))
+(defn symbolize-frame [i next-rolls [head & _]]
+  "pretty prints a frame. frame 10 symbol includes any fill frames"
+  (if (== i 9)
+    (symbolize-frame10 next-rolls)
+    (cond
+      (strike? head) "X"
+      (spare? head) (str (first head) " /")
+      (open? head) (str/join " " head)
+      :else "")))
 
+(defn frame-stat [i [head & rest :as frames]]
+  "returns a map with 3 keys :score, :symbol, :rolls
+  :score - contains the computed score using any following frames
+  :symbol - pretty symbol to represent the score
+  :rolls - All the rolls in encounter order that were used to compute the score
+  "
+  (let [next-rolls (find-next-rolls frames)]
+    {:score (if (empty? next-rolls) missing (reduce + next-rolls))
+     :symbol (if (empty? next-rolls) "" (symbolize-frame i next-rolls frames))
+     :rolls (into [] next-rolls)}))
 
-; (defn full-scorecard [scorecard]
-;   (:result (reduce (fn [acc next]
-;             (-> acc
-;                 (update-in [:result] #(conj % {:score (resolve-frame-score (:scorecard acc)) :symbol (symbolize next)}))
-;                 (update-in [:scorecard] #(rest %)))) {:result [] :scorecard scorecard} scorecard)))
+(defn calculate-scorecard [scorecard]
+  "Given the raw scorecard as a 2d list of frames, calculate the scores and sum the total.
+   If the total is -1 then it means the game is not finished"
+  (let [indexed-frames (map vector (range 0 (count scorecard)) (partition 3 1 scorecard))]
+    (reduce (fn [acc [i [head & _ :as window]]]
+              (let [frame-stat (frame-stat i window)]
+                (-> acc
+                    (update-in [:calculated-scorecard] #(conj % frame-stat))
+                    (update-in [:total] #(if (or (== % missing) (== (:score frame-stat) missing))
+                                           missing
+                                           (+ % (:score frame-stat)))))))
+            {:calculated-scorecard [] :total 0} indexed-frames)))
 
+(defn finished? [calculated-scorecard-result]
+  "true when the game has been finished indicated by a non -1 total score"
+  (not= missing (:total calculated-scorecard-result)))
 
-(defn total-score [scorecard]
-  "returns the sum of 10 valid frames else -1 if no score can be calculated"
-  (reduce (fn [acc next]
-            (println next acc)
-            (let [score (:score next)]
-              (if (or (== acc missing) (== score missing))
-                missing
-                (+ score acc)))) 0 (full-scorecard scorecard)))
-
-
-;; (split-at 9 c)
-
-;; overfil after 10 is 1 element list of every next throw to calculate score for 10
-(def c [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [6 4] [7 3]        [3 5]  [-1 -1]])
-(def d [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [10 -1] [10 -1]    [10 -1] [10 -1]])
-(def e [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [10 -1] [10 -1]    [0 0]   [0 0]])
-(def f [[10 -1] [10 -1] [-2 -1] [10 -1] [10 -1] [10 -1] [10 -1] [10 -1] [10 -1] [10 -1]    [10 -1]   [10 -1]])
+(defn score-frame [scorecard nth frame]
+  "associates the nth frame with a new score and returns the updated scorecard.
+   If the 10th frame is a spare then the 11th frame only needs to contain 1 roll and should be supplied as a vector [roll 0]
+  "
+  (assoc scorecard nth frame))
 
 (defn create-scorecard []
   "2d vector containing 12 frames with with 2 extra to handle strike and spares on the 10th frame"
@@ -107,93 +116,26 @@
        (repeatedly total-frames)
        (into [])))
 
+;; specs
 
 (s/def ::frame (s/and (s/tuple int? int?) valid-frame?))
-(s/def ::scorecard (s/and vector? (s/coll-of vector? :count total-frames)))
+(s/def ::scorecard (s/coll-of vector? :count total-frames))
 
 (s/fdef score-frame
-  :args (s/and (s/cat :scorecard ::scorecard :nth (s/int-in 0 total-frames) :frame ::frame))
-  :ret ::scorecard)
+  :args (s/and (s/cat :scorecard ::scorecard :nth (s/int-in 0 total-frames) :frame ::frame)))
 
+(s/fdef calculate-scorecard
+  :args (s/and (s/cat :scorecard ::scorecard)))
 
 (s/def ::score int?)
-(s/def ::symbol int?)
-(s/def ::score-result (s/keys :req [::score ::symbol]))
-(s/def ::full-scorecard-spec (s/coll-of ::score-result :count total-frames))
+(s/def ::symbol string?)
+(s/def ::total int?)
+(s/def ::rolls any?)
+(s/def ::frame-stat (s/keys :req [::score ::symbol ::rolls]))
+(s/def ::calculated-scorecard (s/coll-of ::frame-stat :count 10))
+(s/def ::calculated-scorecard-result-spec (s/keys :req [::calculated-scorecard ::total]))
 
-(s/fdef full-scorecard
-  :args (s/cat :scorecard ::scorecard)
-  :ret (s/cat ::full-scorecard-spec))
+; (s/fdef finished?
+;   :args (s/cat :calculated-scorecard-result ::calculated-scorecard-result-spec))
 
 (stest/instrument)
-
-
-
-
-; (defn resolve-frame-score
-;   ([[head & tail]]
-;    (let [rolls (cond
-;                  (strike? head) (resolve-frame-score tail 2 [(first head)])
-;                  (spare? head) (resolve-frame-score tail 1 head)
-;                  (open? head) head
-;                  :else [])]
-;      (if (empty? rolls)
-;        missing
-;        (reduce + rolls))))
-;   ([[head & tail] n acc]
-;    (if (or (<= n 0) (empty? head))
-;      acc
-;      (cond
-;        (strike? head) (resolve-frame-score tail (dec n) (conj acc (first head)))
-;        (or (open? head) (spare? head)) (resolve-frame-score tail (- n 2) (into acc (take (min n 2) head)))
-;        :else []))))
-
-; (defn resolve-score
-; "
-; The first frame determines the strategy for finding the scores in the next frames.
-
-; strike - ([10 -1] [10 -1] [10 -1]) => 30 (visit next 2 frames)
-; spare  - ([4 6] [5 1] [10 -1])     => 15 (visit  next)
-; open   - ([9 1] [10 -1] [10 -1])   => 20 
-
-
-; Consider when frame 1 is a strike, the next 2 frames are needed to compute frame 1 score.
-; ([10 -1] [10 -1] [10 -1])
-
-
-; "
-;   ([[head & tail]]
-;    (let [rolls (cond
-;                  (strike? head) (resolve-score tail 2 [(first head)])
-;                  (spare? head) resolve-score tail 1 head)
-;                  (open? head) head
-;                  :else [])]
-;      (if (empty? rolls)
-;        missing
-;        (reduce + rolls))))
-;   ([[head & tail] n acc]
-;   ""
-;    (if (or (<= n 0) (empty? head))
-;      acc
-;      (cond
-;        (strike? head) (resolve-score tail (dec n) (conj acc (first head)))
-;        (or (open? head) (spare? head)) (resolve-score tail (- n 2) (into acc (take (min n 2) head)))
-;        :else []))))
-
-; (defn resolve-frame-score
-;    ([[head & tail]]
-;    (let [rolls (cond
-;                  (strike? head) (resolve-frame-score tail 2 [(first head)])
-;                  (spare? head) (resolve-frame-score tail 1 head)
-;                  (open? head) head
-;                  :else [])]
-;      (if (empty? rolls)
-;        missing
-;        (reduce + rolls))))
-;   ([[head & tail] n acc]
-;    (if (or (<= n 0) (empty? head))
-;      acc
-;      (cond
-;        (strike? head) (resolve-frame-score tail (dec n) (conj acc (first head)))
-;        (or (open? head) (spare? head)) (resolve-frame-score tail (- n 2) (into acc (take (min n 2) head)))
-;        :else []))))
--- test/tenpin/core_test.clj
+++ test/tenpin/core_test.clj
@@ -1,30 +1,206 @@
 (ns tenpin.core-test
   (:require [clojure.test :refer :all]
-            [tenpin.core :refer :all]))
+            [tenpin.core :refer :all]
+            [clojure.spec.alpha :as s]))
 
-(deftest strike
-  (testing "strike when first roll is 10"
-    (is (true? (strike? [10 0])))
-    (is (true? (strike? [10 1])))
-    (is (true? (strike? [10 10])))
-    (is (true? (strike? [10 10 10 10])))
-    (is (false? (strike? [0 0])))
-    (is (false? (strike? [0 1])))
-    (is (false? (strike? [5 5])))
-    (is (false? (strike? [3 4])))
-    (is (false? (strike? [0 10])))))
+(deftest legal-rolls-scenarios
+  (testing "single legal roll must be between 0 - 10 inclusive"
+    (is (every? #(legal-roll? %) [0 5 10]))
+    (is (every? #(not (legal-roll? %)) [-1 11])))
+  (testing "both rolls must be between 0 - 10 inclusive"
+    (is (every? #(two-legal-rolls? %) [[0 0] [10 10] [1 9] [9 1]]))
+    (is (every? #(not (two-legal-rolls? %)) [[0 11] [11 0] [-1 10] [10 -1]]))))
 
-(deftest spare
-  (testing "spare when 2 rolls sum to 10"
-    (is (true? (spare? [10 0])))
-    (is (true? (spare? [0 10])))
-    (is (true? (spare? [5 5])))
-    (is (true? (spare? [1 9])))
-    (is (true? (spare? [9 1])))
-    (is (true? (spare? [9 1 1])))
-    (is (true? (spare? [10 0 10])))
-    (is (false? (spare? [0 0])))
-    (is (false? (spare? [1 0])))
-    (is (false? (spare? [0 1])))
-    (is (false? (spare? [10])))
-    (is (false? (spare? [3 4])))))
+(deftest strike-scenarios
+  (testing "strike only when frame is [10 -1]"
+    (is (true? (strike? [10 -1])))
+    (is (every? #(not (strike? %)) [[10 10] [-1 10] [0 10] [10 0]]))))
+
+(deftest spare-scenarios
+  (testing "frame is a spare when both rolls are between 0 and 10 inclusive and sum to exactly 10"
+    (is (every? #(spare? %) [[10 0] [0 10] [5 5] [1 9] [9 1]]))
+    (is (every? #(not (spare? %)) [[-1 11] [15 -5] [0 0] [1 0] [0 1] [3 4]]))))
+
+(deftest open-scenarios
+  (testing "frame is open when both rolls are between 0 and 10 inclusive and sum to less than 10"
+    (is (every? #(open? %) [[0 0] [9 0] [0 9] [1 8] [7 2]]))
+    (is (every? #(not (open? %)) [[-1 -1] [10 0] [0 10] [5 5] [1 9] [9 1] [-1 11] [-2 11] [15 -5] [15 -6]]))))
+
+(deftest valid-frame-scenarios
+  (testing "a frame is valid if its either open, spare or a strike else false"
+    (is (every? #(valid-frame? %) [[10 -1] [3 7] [0 0] [7 2]]))
+    (is (every? #(not (valid-frame? %)) [[-1 -1] [10 10] [5 6] [0 -1]]))))
+
+(deftest score-frame-scenarios
+  (testing "updates the scorecard by replacing the nth frame"
+    (is (= [[0 0] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1]]
+           (score-frame (create-scorecard) 0 [0 0])) "frame 0 did not get updated")
+    (is (= [[-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [0 0]]
+           (score-frame (create-scorecard) 11 [0 0])) "frame 12 did not get updated")))
+
+(deftest find-next-rolls-scenarios
+  (testing "when frame 1 is missing"
+    (is (= [] (find-next-rolls [[-1 -1] [-1 -1] [-1 -1]]))))
+  (testing "when frame 1 is open"
+    (is (= [0 0] (find-next-rolls [[0 0] [-1 -1] [-1 -1]])))
+    (is (= [5 4] (find-next-rolls [[5 4] [-1 -1] [-1 -1]]))))
+  (testing "when frame 1 is a strike"
+    (is (= [] (find-next-rolls [[10 -1] [-1 -1] [-1 -1]])) "strike needs 2 more rolls")
+    (is (= [] (find-next-rolls [[10 -1] [10 -1] [-1 -1]])) "strike needs 1 more roll")
+    (is (= [10 10 10] (find-next-rolls [[10 -1] [10 -1] [10 -1]])) "3 strikes")
+    (is (= [10 10 1] (find-next-rolls [[10 -1] [10 -1] [1 0]])) "2 strikes then use first open roll")
+    (is (= [10 10 0] (find-next-rolls [[10 -1] [10 -1] [0 10]])) "2 strikes then use first open roll")
+    (is (= [10 0 0] (find-next-rolls [[10 -1] [0 0] [10 -1]])) "strike then use both of frame 2 rolls")
+    (is (= [10 3 2] (find-next-rolls [[10 -1] [3 2] [10 -1]])) "strike then use both of frame 2 rolls")
+    (is (= [10 3 7] (find-next-rolls [[10 -1] [3 7] [10 -1]])) "strike then spare"))
+  (testing "when frame 1 is a spare"
+    (is (= [] (find-next-rolls [[7 3] [-1 -1] [-1 -1]])) "spare needs 1 more roll")
+    (is (= [1 9 0] (find-next-rolls [[1 9] [0 0] [-1 -1]])) "spare uses first roll of next frame")
+    (is (= [1 9 9] (find-next-rolls [[1 9] [9 1] [-1 -1]])) "spare uses first roll of next frame")
+    (is (= [8 2 10] (find-next-rolls [[8 2] [10 -1] [10 -1]])) "spare uses first roll of next frame")))
+
+(deftest calculate-scorecard-scenarios
+  (testing "empty score scard has -1 score"
+    (let [scorecard (into [] (repeatedly 12 (constantly [-1 -1])))
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= -1 (:total calculated-scorecard-result)))
+      (is (false? (finished? calculated-scorecard-result)))))
+  (testing "all gutter balls should be 0 total"
+    (let [scorecard (into [] (repeatedly 12 (constantly [0 0])))
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= 0 (:total calculated-scorecard-result)))
+      (is (true? (finished? calculated-scorecard-result)))))
+  (testing "all strikes should have the max total of 300"
+    (let [scorecard (into [] (repeatedly 12 (constantly [10 -1])))
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 30 :symbol "X X X" :rolls [10 10 10]}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= 300 (:total calculated-scorecard-result)))
+      (is (true? (finished? calculated-scorecard-result)))))
+  (testing "all spares with 1 open fill frame that must have its second roll as zero"
+    (let [scorecard [[9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [5 0] [-1 -1]]
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 15 :symbol "9 / 5" :rolls [9 1 5]}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= 186 (:total calculated-scorecard-result)))
+      (is (true? (finished? calculated-scorecard-result)))))
+  (testing "all spares with 1 overfill strike"
+    (let [scorecard [[9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [9 1] [10 -1] [-1 -1]]
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 19 :symbol "9 /" :rolls [9 1 9]}
+              {:score 20 :symbol "9 / X" :rolls [9 1 10]}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= 191 (:total calculated-scorecard-result)))
+      (is (true? (finished? calculated-scorecard-result)))))
+  (testing "sample game with spare overfil"
+    (let [scorecard [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [6 4] [7 3] [3 0] [-1 -1]]
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 20 :symbol "X" :rolls [10 7 3]}
+              {:score 17 :symbol "7 /" :rolls [7 3 7]}
+              {:score 9 :symbol "7 2" :rolls [7 2]}
+              {:score 20 :symbol "9 /" :rolls [9 1 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 22 :symbol "X" :rolls [10 10 2]}
+              {:score 15 :symbol "X" :rolls [10 2 3]}
+              {:score 5 :symbol "2 3" :rolls [2 3]}
+              {:score 17 :symbol "6 /" :rolls [6 4 7]}
+              {:score 13 :symbol "7 / 3" :rolls [7 3 3]}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= 168 (:total calculated-scorecard-result)))
+      (is (true? (finished? calculated-scorecard-result)))))
+  (testing "sample game with no overfills"
+    (let [scorecard [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [6 4] [0 0] [-1 -1] [-1 -1]]
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 20 :symbol "X" :rolls [10 7 3]}
+              {:score 17 :symbol "7 /" :rolls [7 3 7]}
+              {:score 9 :symbol "7 2" :rolls [7 2]}
+              {:score 20 :symbol "9 /" :rolls [9 1 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 22 :symbol "X" :rolls [10 10 2]}
+              {:score 15 :symbol "X" :rolls [10 2 3]}
+              {:score 5 :symbol "2 3" :rolls [2 3]}
+              {:score 10 :symbol "6 /" :rolls [6 4 0]}
+              {:score 0 :symbol "0 0" :rolls [0 0]}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= 148 (:total calculated-scorecard-result)))
+      (is (true? (finished? calculated-scorecard-result)))))
+  (testing "sample game with 2 strike overfils"
+    (let [scorecard [[10 -1] [7 3] [7 2] [9 1] [10 -1] [10 -1] [10 -1] [2 3] [6 4] [10 -1] [10 -1] [9 0]]
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 20 :symbol "X" :rolls [10 7 3]}
+              {:score 17 :symbol "7 /" :rolls [7 3 7]}
+              {:score 9 :symbol "7 2" :rolls [7 2]}
+              {:score 20 :symbol "9 /" :rolls [9 1 10]}
+              {:score 30 :symbol "X" :rolls [10 10 10]}
+              {:score 22 :symbol "X" :rolls [10 10 2]}
+              {:score 15 :symbol "X" :rolls [10 2 3]}
+              {:score 5 :symbol "2 3" :rolls [2 3]}
+              {:score 20 :symbol "6 /" :rolls [6 4 10]}
+              {:score 29 :symbol "X X 9" :rolls [10 10 9]}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= 187 (:total calculated-scorecard-result)))
+      (is (true? (finished? calculated-scorecard-result)))))
+  (testing "sample game that has been started but not finished"
+    (let [scorecard [[10 -1] [7 3] [7 2] [9 1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1] [-1 -1]]
+          calculated-scorecard-result (calculate-scorecard scorecard)]
+      (is (= [{:score 20 :symbol "X" :rolls [10 7 3]}
+              {:score 17 :symbol "7 /" :rolls [7 3 7]}
+              {:score 9 :symbol "7 2" :rolls [7 2]}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}
+              {:score -1 :symbol "" :rolls []}]
+             (:calculated-scorecard calculated-scorecard-result)))
+      (is (= -1 (:total calculated-scorecard-result)))
+      (is (false? (finished? calculated-scorecard-result))))))
